cf21125b7066edfa9c78f44e491dfbe7e5cf2658
Jonathan Vollebregt <jnvsor@gmail.com>
2015-12-18 20:09:19 +0100

    Added automatic detection of required extension based on php tags

--- _plugins/jekyll-php.rb
+++ _plugins/jekyll-php.rb
@@ -2,14 +2,14 @@ module Jekyll
   class Page
     def destination(dest)
       path = site.in_dest_dir(dest, URL.unescape_path(url))
-      if url.end_with?("/")
-        if output_ext == ".php"
-          path = File.join(path, "index.php")
-        else
-          path = File.join(path, "index.html")
-        end
+
+      if output and /<\?(=.+\?>|php)/i.match(output)
+        path = File.join(path, "index.php") if url.end_with?("/")
+      else
+        path = File.join(path, "index.html") if url.end_with?("/")
+        path << output_ext unless path.end_with?(output_ext)
       end
-      path << output_ext unless path.end_with?(output_ext)
+
       path
     end
   end
